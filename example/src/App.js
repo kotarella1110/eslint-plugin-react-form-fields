ccb378474c041b9e09eaa44bfa94d5719145fae2
Kotaro Sugawara <kotarella1110@gmail.com>
2021-04-06 17:04:38 +0900

    chore(example): add

---
+++ example/src/App.js
@@ -0,0 +1,26 @@
+function App() {
+  return (
+    <>
+      <input />
+      <input type="text" defaultValue="test" />
+      <input type="checkbox" defaultChecked={true} />
+      <input type="radio" defaultChecked={true} />
+      <textarea />
+      <textarea defaultValue="test" />
+      <select />
+      <select defaultValue="test" />
+      <input type="text" value="test" />
+      <input type="checkbox" checked={true} />
+      <input type="radio" checked={true} />
+      <textarea value="test" />
+      <select value="test" />
+      <input type="text" value="test" defaultValue="test" />',
+      <input type="checkbox" checked={true} defaultChecked={true} />',
+      <input type="radio" checked={true} defaultChecked={true} />',
+      <textarea value="test" defaultValue="test" />',
+      <select type="text" value="test" defaultValue="test" />',
+    </>
+  );
+}
+
+export default App;
